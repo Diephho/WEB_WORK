893fa8bf4dab2ac9c6864a2a93ffafce2c21dedb
Diephho <okdiep1@gmail.com>
2024-05-13 01:55:32 +0700

    upt: webp

--- DjangoApp/models.py
+++ DjangoApp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
 from django_resized  import ResizedImageField
+from PIL import Image, ExifTags
+
 # Create your models here.
 class UserInfo(models.Model):
     id=models.IntegerField(primary_key=True)
@@ -28,7 +30,7 @@ class Post(models.Model):
     star = models.FloatField(default=0)
     address = models.CharField(max_length=100)
     image = ResizedImageField(size=[700, 400], blank=True, null=True,quality=90)
-    imagePhone = ResizedImageField(size=[300, 200], blank=True, null=True,quality=70, upload_to="phone")
+    imagePhone = ResizedImageField(size=[300, 200], blank=True, null=True,quality=70, upload_to="phone",force_format='WEBP')
     lat = models.CharField(max_length=200,blank=True,null=True)
     lng = models.CharField(max_length=200,blank=True,null=True)
     idUser = models.ForeignKey("UserInfo", on_delete=models.CASCADE, null= True)
@@ -37,7 +39,26 @@ class Post(models.Model):
     totalstar=models.IntegerField(default=0)
     def __str__(self):
         return self.title
+    def save(self, *args, **kwargs):
+        if self.imagePhone:
+            # Open the image using Pillow
+            image = Image.open(self.imagePhone)
+            
+            # Check for EXIF data and rotation information
+            if hasattr(image, '_getexif'):
+                exif = image._getexif()
+                if exif and ExifTags.TAGS.get(orientation := exif.get(0x0112)):
+                    if orientation == 3:
+                        image = image.rotate(180, expand=True)
+                    elif orientation == 6:
+                        image = image.rotate(270, expand=True)
+                    elif orientation == 8:
+                        image = image.rotate(90, expand=True)
+            
+            # Save the modified image
+            image.save(self.imagePhone.path, 'WEBP')
 
+        super().save(*args, **kwargs)
 class Comment(models.Model):
     id=models.AutoField(primary_key=True)
     content=models.CharField(max_length=200)
